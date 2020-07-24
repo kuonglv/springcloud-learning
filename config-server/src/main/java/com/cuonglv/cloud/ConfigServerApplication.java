83b91a7fc8f9ecc91a1c1577c180252e15f1700b
cuonglv <cuonglv@itsol.vn>
2020-07-24 14:36:07 +0700

    Update config

--- config-server/src/main/java/com/cuonglv/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/cuonglv/cloud/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.macro.cloud;
+package com.cuonglv.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-server/src/test/java/com/cuonglv/cloud/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/cuonglv/cloud/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.macro.cloud;
+package com.cuonglv.cloud;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
